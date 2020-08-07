b67ba67b8b8e93396a25149127d3985d8e4a455e
DaevMithran <daevmithran1999@gmail.com>
2020-08-07 14:35:49 +0530

    Angry_Birds

---
+++ 2D_AngryBirds/Assets/Scripts/Bird.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Bird : MonoBehaviour
+{
+    private bool _birdlaunced;
+    Vector3 _initialPosition;
+    private float _timerest;
+
+    [SerializeField] private float speed = 500f;
+    
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        GetComponent<LineRenderer>().SetPosition(1, _initialPosition);
+        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        if (_birdlaunced && GetComponent<Rigidbody2D>().velocity.magnitude < 0.1)
+            _timerest += Time.deltaTime; 
+        if (transform.position.y > 2.4 || transform.position.x > 14  || _timerest > 2)
+            SceneManager.LoadScene(0);
+    }
+    
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        GetComponent<LineRenderer>().enabled = true;
+    }
+
+    private void OnMouseUp()
+    { 
+        GetComponent<SpriteRenderer>().color = Color.white;
+        Vector2 dirToinitial = _initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(dirToinitial * speed);
+        GetComponent<Rigidbody2D>().gravityScale = 0.75f;
+        _birdlaunced = true;
+        GetComponent<LineRenderer>().enabled = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y, 0);
+    }
+
+}
---
+++ 2D_AngryBirds/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+  
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Animator anim = GetComponent<Animator>();
+        Bird hit = collision.collider.GetComponent<Bird>();
+        if (hit!=null)
+        {
+            //GameObject.Destroy(gameObject);
+            anim.SetTrigger("Die");
+            return;
+        }
+
+        if (collision.collider.GetComponent<Enemy>() != null)
+        {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y < -0.5)
+        {
+            //GameObject.Destroy(gameObject);
+            anim.SetTrigger("Die");
+            return;
+        }
+
+
+    }
+}
